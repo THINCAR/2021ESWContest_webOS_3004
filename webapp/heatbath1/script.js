3fb93e141bc6dad93bdb359d5dadc311f3e674d8
wldus <fineje99@naver.com>
2021-08-28 23:19:21 +0900

    simplify javascript

--- webapp/heatbath1/script.js
+++ webapp/heatbath1/script.js
@@ -1,6 +1,5 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
 
 function doShow1() { 
     if ($('#aa').is(":visible")) {  
@@ -27,10 +26,6 @@ closeBtn.addEventListener("click", ()=>{
     menuBtnChange();
 });
 
-searchBtn.addEventListener("click", ()=>{ 
-    sidebar.classList.toggle("open");
-    menuBtnChange(); 
-});
 
 function menuBtnChange() {
     if(sidebar.classList.contains("open")){
