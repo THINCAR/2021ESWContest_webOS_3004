44b8004ab5b55685d58505fb642bb33c1538ee0a
wldus <fineje99@naver.com>
2021-09-04 18:16:10 +0900

    Update: fix the door design

--- webapp/door1/script.js
+++ webapp/door1/script.js
@@ -2,31 +2,49 @@ function select(num) {
     document.getElementById("audio").src = './uploads/result' + num + '.wav'
 }
 
+$('#aa').show();
+$('#bb').hide();
+$('#cc').hide(); 
+
+function doShow1(){
+    if ($('#aa').is(":visible")) { 
+        
+    }   
+    else { 
+        $('#cc').hide(); 
+        $('#bb').hide();
+        $('#aa').show(); 
+    } 
+}
+function doShow2(){
+    if ($('#bb').is(":visible")) { 
+        
+    }   
+    else { 
+        $('#aa').hide(); 
+        $('#cc').hide();
+        $('#bb').show(); 
+    } 
+}
+function doShow3(){
+    if ($('#cc').is(":visible")) { 
+        
+    }   
+    else { 
+        $('#aa').hide(); 
+        $('#bb').hide();
+        $('#cc').show(); 
+    } 
+}
 window.onload = function() {
+
+
     module_link = "http://192.168.0.35/"
 
-    let sidebar = document.querySelector(".sidebar");
-    let closeBtn = document.querySelector("#btn");
-    let menu1 = document.querySelector("#menu_1");
-    let menu2 = document.querySelector("#menu_2");
-    let menu3 = document.querySelector("#menu_3");
 
     let btn_a = document.querySelector("#button_a")
     let btn_b = document.querySelector("#button_b")
 
-    let page1 = document.querySelector("#aa");
-    let page2 = document.querySelector("#bb");
-    let page3 = document.querySelector("#cc");
-
-
-    function menuBtnChange() {
-        if(sidebar.classList.contains("open")){
-            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-        }
-        else {
-            closeBtn.classList.replace("bx-menu-alt-right","bx-menu");
-        }
-    }
 
     for (i=0; i<10; i++) {
         file = "./uploads/name" + i + ".txt";
@@ -75,44 +93,5 @@ window.onload = function() {
         }
         rawFile.send(null);
     }
-  
-    closeBtn.addEventListener("click", ()=>{
-        sidebar.classList.toggle("open");
-        menuBtnChange();
-    });
-
-    $('#aa').show();
-    $('#bb').hide();
-    $('#cc').hide(); 
-
-    menu1.addEventListener("click",()=>{
-        if ($('#aa').is(":visible")) {  
-            
-        }   
-        else {
-          $('#bb').hide();
-          $('#aa').show();
-          $('#cc').hide();  
-        } 
-    });
-    menu2.addEventListener("click",()=>{
-        if ($('#bb').is(":visible")) { 
-            
-        }   
-        else { 
-            $('#aa').hide();
-            $('#cc').hide(); 
-            $('#bb').show(); 
-        } 
-    });
-    menu3.addEventListener("click",()=>{
-        if ($('#cc').is(":visible")) { 
-        
-        }   
-        else { 
-            $('#aa').hide(); 
-            $('#bb').hide();
-            $('#cc').show(); 
-        } 
-    });
+   
 }
